18492acfc367534ffccd70e5098a7091bde3ac52
WISKAS <shevchenkozakhar5@gmail.com>
2026-01-29 22:58:51 +0700

    Update start.js

--- start.js
+++ start.js
@@ -7,11 +7,21 @@ document.addEventListener('DOMContentLoaded', () => {
     video.muted = true;
     video.pause();
 
+    const linkSound = new Audio("link_sound.mp3");
+    linkSound.volume = 0.25;
+
     function startSite() {
         video.muted = false;
         video.volume = 0.1;
-
         video.play().catch(() => {});
+
+        linkSound.play().then(() => {
+            linkSound.pause();
+            linkSound.currentTime = 0;
+
+            window.__linkSoundUnlocked = true;
+            window.__linkSoundBase = linkSound;
+        }).catch(() => {});
 
         startScreen.classList.add('hidden');
 
